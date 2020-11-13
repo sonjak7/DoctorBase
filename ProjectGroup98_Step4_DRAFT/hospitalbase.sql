13ce83b56c93e742e5befd9c7ece1529fdddd0c8
sonjak7 <sonjak737@gmail.com>
2020-11-13 15:46:46 -0800

    finalized sql queries

--- ProjectGroup98_Step4_DRAFT/hospitalbase.sql
+++ ProjectGroup98_Step4_DRAFT/hospitalbase.sql
@@ -6,80 +6,92 @@
 
 /* Create a table for Doctors */
 
-DROP TABLE IF EXISTS `doctors`;
+DROP TABLE IF EXISTS `Doctors`;
 
-CREATE TABLE `doctors` (
-  `doctorid` int(11) NOT NULL AUTO_INCREMENT,
-  `lastname` varchar(255) NOT NULL,
-  `firstname` varchar(255) NOT NULL,
+CREATE TABLE `Doctors` (
+  `doctorID` int(11) NOT NULL AUTO_INCREMENT,
+  `lastName` varchar(255) NOT NULL,
+  `firstName` varchar(255) NOT NULL,
   `department` varchar(255) NOT NULL,
-  PRIMARY KEY (`doctorid`)
+  PRIMARY KEY (`doctorID`)
 );
 
-/* Create a table for staff */
+/* Create a table for Staff */
 
-DROP TABLE IF EXISTS `staff`;
+DROP TABLE IF EXISTS `Staff`;
 
-CREATE TABLE `staff` (
-  `staffid` int(11) NOT NULL AUTO_INCREMENT,
-  `stafftype` varchar(255) NOT NULL,
-  `lastname` varchar(255) NOT NULL,
-  `firstname` varchar(255) NOT NULL,
-  PRIMARY KEY (`staffid`)
+CREATE TABLE `Staff` (
+  `staffID` int(11) NOT NULL AUTO_INCREMENT,
+  `staffType` varchar(255) NOT NULL,
+  `lastName` varchar(255) NOT NULL,
+  `firstName` varchar(255) NOT NULL,
+  PRIMARY KEY (`staffID`)
 );
 
-/* Create a table for patients */
+/* Create a table for Patients */
 
-DROP TABLE IF EXISTS `patients`;
+DROP TABLE IF EXISTS `Patients`;
 
-CREATE TABLE `patients` (
-  `patientid` int(11) NOT NULL AUTO_INCREMENT,
-  `lastname` varchar(255) NOT NULL,
-  `firstname` varchar(255) NOT NULL,
-  `primarydoctorid` int(11) NOT NULL,
-  PRIMARY KEY (`patientid`),
-  FOREIGN KEY (`primarydoctorid`) REFERENCES `doctors` (`doctorid`)
+CREATE TABLE `Patients` (
+  `patientID` int(11) NOT NULL AUTO_INCREMENT,
+  `lastName` varchar(255) NOT NULL,
+  `firstName` varchar(255) NOT NULL,
+  `primaryDoctorID` int(11) NOT NULL,
+  PRIMARY KEY (`patientID`),
+  FOREIGN KEY (`primaryDoctorID`) REFERENCES `Doctors` (`doctorID`)
 );
 
 
-/* Create a table for orders */
+/* Create a table for Orders */
 
-DROP TABLE IF EXISTS `orders`;
+DROP TABLE IF EXISTS `Orders`;
 
-CREATE TABLE `orders` (
-  `orderid` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `Orders` (
+  `orderID` int(11) NOT NULL AUTO_INCREMENT,
   `date` date NOT NULL,
   `time` time NOT NULL,
-  `ordertype` varchar(255) NOT NULL,
-  `patientid` int(11) NOT NULL,
-  `doctorid` int(11) NOT NULL,
-  `staffid` int(11) NULL,
-  PRIMARY KEY (`orderid`),
-  FOREIGN KEY (`patientid`) REFERENCES `patients` (`patientid`),
-  FOREIGN KEY (`doctorid`) REFERENCES `doctors` (`doctorid`),
-  FOREIGN KEY (`staffid`) REFERENCES `staff` (`staffid`)
+  `orderType` varchar(255) NOT NULL,
+  `patientID` int(11) NOT NULL,
+  `doctorID` int(11) NOT NULL,
+  `staffID` int(11) NULL,
+  PRIMARY KEY (`orderID`),
+  FOREIGN KEY (`patientID`) REFERENCES `Patients` (`patientID`),
+  FOREIGN KEY (`doctorID`) REFERENCES `Doctors` (`doctorID`),
+  FOREIGN KEY (`staffID`) REFERENCES `Staff` (`staffID`)
 );
 
-/* Create a table for results */
+/* Create a table for Results */
 
-DROP TABLE IF EXISTS `results`;
+DROP TABLE IF EXISTS `Results`;
 
-CREATE TABLE `results` (
-  `resultid` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `Results` (
+  `resultID` int(11) NOT NULL AUTO_INCREMENT,
   `status` varchar(255) NOT NULL,
-  `orderid` int(11) NOT NULL,
-  `date` date NOT NULL,
-  `accessedbydoctor` bit(1) DEFAULT b'0',
-  PRIMARY KEY (`resultid`),
-  FOREIGN KEY (`orderid`) REFERENCES `orders` (`orderid`)
+  `orderID` int(11) NOT NULL,
+  `date` date NULL,
+  `accessedByDoctor` bit(1) DEFAULT b'0',
+  PRIMARY KEY (`resultID`),
+  FOREIGN KEY (`orderID`) REFERENCES `Orders` (`orderID`)
+);
+
+/* Create a table for Doctors_Patients relationship */
+
+DROP TABLE IF EXISTS `Doctors_Patients`;
+
+CREATE TABLE `Doctors_Patients` (
+  `doctorID` int(11) NOT NULL,
+  `patientID` int(11) NOT NULL,
+  PRIMARY KEY (`doctorID`, `patientID`),
+  FOREIGN KEY (`doctorID`) REFERENCES `Doctors` (`doctorID`),
+  FOREIGN KEY (`patientID`) REFERENCES `Patients` (`patientID`)
 );
 
 /* (b) Sample Data */
 
-/* Insert into doctors --> 5 doctors */
+/* Insert into Doctors --> 5 doctors */
 
-LOCK TABLES `doctors` WRITE;
-INSERT INTO `doctors` VALUES
+LOCK TABLES `Doctors` WRITE;
+INSERT INTO `Doctors` VALUES
 (1,'Carson','Ben', 'Neurology'),
 (2,'Mukherjee','Siddharth', 'Oncology'),
 (3,'Gawande','Atul', 'Endocrinology'),
@@ -87,10 +99,10 @@ INSERT INTO `doctors` VALUES
 (5,'of Kos','Hippocrates', 'Internal Medicine');
 UNLOCK TABLES;
 
-/* Insert into patients --> 5 patients */
+/* Insert into Patients --> 5 patients */
 
-LOCK TABLES `patients` WRITE;
-INSERT INTO `patients` VALUES
+LOCK TABLES `Patients` WRITE;
+INSERT INTO `Patients` VALUES
 (1,'Bechdel','Alison', 4),
 (2,'McCraney','Tarell', 2),
 (3,'Diaz','Junot', 5),
@@ -98,10 +110,10 @@ INSERT INTO `patients` VALUES
 (5,'Duflo','Esther', 1);
 UNLOCK TABLES;
 
-/* Insert into staff --> 5 staff */
+/* Insert into Staff --> 5 staff */
 
-LOCK TABLES `staff` WRITE;
-INSERT INTO `staff` VALUES
+LOCK TABLES `Staff` WRITE;
+INSERT INTO `Staff` VALUES
 (1,'pathology', 'Pasteur','Louis'),
 (2,'nursing','Nightingale','Florence'),
 (3,'radiology', 'Damadian','Raymond'),
@@ -109,10 +121,10 @@ INSERT INTO `staff` VALUES
 (5,'pharmacy', 'Pankiewicz','Tadeusz');
 UNLOCK TABLES;
 
-/* Insert into orders --> 5 orders */
+/* Insert into Orders --> 5 orders */
 
-LOCK TABLES `orders` WRITE;
-INSERT INTO `orders` VALUES
+LOCK TABLES `Orders` WRITE;
+INSERT INTO `Orders` VALUES
 (1,'2020-11-12','08:30:00', 'prescription', 1, 4, 4),
 (2,'2020-11-12','08:30:00', 'blood tests', 2, 2, 1),
 (3,'2020-11-12','08:30:00', 'prescription', 3, 5, 5),
@@ -121,13 +133,20 @@ INSERT INTO `orders` VALUES
 UNLOCK TABLES;
 
 
-/* Insert into results --> 5 results */
+/* Insert into Results --> 5 results */
 
-LOCK TABLES `results` WRITE;
-INSERT INTO `results` VALUES
+LOCK TABLES `Results` WRITE;
+INSERT INTO `Results` VALUES
 (1,'fulfilled', 1, '2020-11-15', ''),
 (2,'pending: patient visit', 2, '2020-11-15', ''),
 (3,'pending: order received', 3, '2020-11-15', ''),
 (4,'fulfilled', 4, '2020-11-15', ''),
 (5,'fulfilled', 5, '2020-11-15', '');
+UNLOCK TABLES;
+
+/*Insert into Doctors_Patients --> 5 relations*/
+
+LOCK TABLES `Doctors_Patients` WRITE;
+INSERT INTO `Doctors_Patients` VALUES
+(1, 3), (1, 5), (3, 2), (4, 4), (2, 3);
 UNLOCK TABLES;
--- ProjectGroup98_Step4_DRAFT/hospitalbase_data_manipulation.sql
+++ ProjectGroup98_Step4_DRAFT/hospitalbase_data_manipulation.sql
@@ -9,62 +9,126 @@ denote the variables that will have data from the backend programming language
 /* SELECT, INSERT for all tables */
 
 -- Doctors Table
+-- get all doctors' id, first name, last name, and department for listing all doctors
+SELECT * FROM Doctors;
+
 -- search doctors by their first and last name
-SELECT doctors.doctorid FROM doctors
-WHERE doctors.firstname = :firstnameInput AND doctors.lastname = :lastnameInput;
+SELECT Doctors.doctorID FROM Doctors
+WHERE Doctors.firstName = :firstNameInput AND Doctors.lastName = :lastNameInput;
 
 -- add new doctor
-INSERT INTO doctors (lastname, firstname, department)
-VALUES (:lastnameInput, :firstnameInput, :departmentInput);
+INSERT INTO Doctors (lastName, firstName, department)
+VALUES (:lastNameInput, :firstNameInput, :departmentInput);
 
 -- Patients Table
+--get all patients' first name, last name, and primary doctor's last name for listing all patients
+SELECT p.patientID, p.firstName, p.lastName, d.lastName AS "Primary doctor's name"
+FROM Patients p INNER JOIN
+Doctors d ON p.primaryDoctorID = d.doctorID;
+
 -- look up patient by their first and last name
-SELECT patients.patientid FROM patients
-WHERE patients.firstname = :firstnameInput AND patients.lastname = :lastnameInput;
+SELECT Patients.patientID FROM Patients
+WHERE Patients.firstName = :firstNameInput AND Patients.lastName = :lastNameInput;
 
 -- add a new patient
-INSERT INTO patients (lastname, firstname, primarydoctorid)
-VALUES (:lastnameInput, :firstnameInput, :primarydoctoridInput);
+INSERT INTO Patients (lastName, firstName, primaryDoctorID)
+VALUES (:lastNameInput, :firstNameInput, :primaryDoctorIDInput);
 
 -- Staff Table
+--get all staff' first name, last name, and type for listing all staff
+SELECT * FROM Staff;
+
 -- search staff by their first and last name
-SELECT staff.staffid FROM staff
-WHERE staff.firstname = :firstnameInput AND staff.lastname = :lastnameInput;
+SELECT Staff.staffID FROM Staff
+WHERE Staff.firstName = :firstNameInput AND Staff.lastName = :lastNameInput;
 
 -- add a new staff
-INSERT INTO staff (stafftype, lastname, firstname)
-VALUES (:stafftypeInput, :lastnameInput, :firstnameInput);
+INSERT INTO Staff (staffType, lastName, firstName)
+VALUES (:staffTypeInput, :lastNameInput, :firstNameInput);
 
 -- Orders Table
--- seach orders by orderid
-SELECT orders.orderid FROM orders
-WHERE orders.orderid = :orderidInput;
+--get all order's date, time, type, associated patient's, doctor's, and staff's last name (if exists)
+SELECT o.orderID, o.date, o.time, o.orderType, p.lastName AS "Patient's Name",
+d.lastName AS "Doctor's Name", s.lastName AS "Staff's Name" FROM Orders o 
+INNER JOIN Patients p ON o.patientID = p.patientID
+INNER JOIN Doctors d ON o.doctorID = d.doctorID
+INNER JOIN Staff s ON o.staffID = s.staffID;
 
--- add new order (NOTE: staffid is optional)
-INSERT INTO orders (date, time, ordertype, patientid, doctorid, staffid)
-VALUES (:dateInput, :timeInput, :ordertypeInput, :patientidInput, :doctoridInput, :staffidInput);
+-- seach Orders by orderID
+SELECT Orders.orderID FROM Orders
+WHERE Orders.orderID = :orderIDInput;
+
+-- add new order (NOTE: staffID is optional)
+INSERT INTO Orders (date, time, orderType, patientID, doctorID, staffID)
+VALUES (:dateInput, :timeInput, :orderTypeInput, :patientIDInput, :doctorIDInput, :staffIDInput);
 
 -- Results Table
+--get all result's status, date, `accessedByDoctor`, and associated order type
+SELECT * FROM Results;
+
 -- get the results of an order
-SELECT results.resultid FROM results
-WHERE results.orderid = :orderidInput;
+SELECT Results.resultID FROM Results
+WHERE Results.orderID = :orderIDInput;
 
 -- insert result into order (though this is mostly handled with update)
-INSERT INTO results (status, orderid, date, accessedbydoctor)
-VALUES (:statusInput, :orderidInput, :dateInput, :accessedbydoctorInput);
+INSERT INTO Results (status, orderID, date, accessedByDoctor)
+VALUES (:statusInput, :orderIDInput, :dateInput, :accessedByDoctorInput);
+
+-- Doctors_Patients Table
+-- get all doctor-patient relationships, list doctor's last name and patient's first/last name
+SELECT d.lastName AS "Doctor's name", CONCAT(p.firstName, ' ', p.lastName) AS "Patient's Name"
+FROM Doctors_Patients dp
+INNER JOIN Doctors d ON d.doctorID = dp.doctorID
+INNER JOIN Patients p ON p.patientID = dp.patientID;
+
+-- insert new relationship between patient and doctor
+INSERT INTO Doctors_Patients (doctorID, patientID)
+VALUES (:doctorIDInput, :patientIDInput);
 
 
-/* UPDATE, DELETE For Orders and Results*/
+/* UPDATE, DELETE For all tables*/
 
 -- update the status of an order
-UPDATE results
-SET results.status = :statusInput
-WHERE results.orderid = :orderidInput;
+UPDATE Results
+SET Results.status = :statusInput
+WHERE Results.orderID = :orderIDInput;
 
--- update the staffid for an order (when staff chooses to work on an order)
-UPDATE orders
-SET orders.staffid = :staffid
-WHERE orders.orderid = :orderidInput;
+-- update the staffID for an order (when staff chooses to work on an order)
+UPDATE Orders
+SET Orders.staffID = :staffIDInput
+WHERE Orders.orderID = :orderIDInput;
 
 -- delete an order
-DELETE FROM torders WHERE orders.orderid = :orderidInput;
+DELETE FROM Orders WHERE Orders.orderID = :orderIDInput;
+
+/*
+NOTE: For updating doctors, patients, and staff, only what needs to be changed will be changed. Front-end
+should automatically fill in old information into text fields to allow this to happen
+*/
+
+--update a doctor(change of department, name, etc.)
+UPDATE Doctors
+SET firstName = :firstNameInput, lastName = :lastNameInput, department = :departmentInput
+WHERE doctorID = :doctorIDInput; 
+
+--update a patient(change of primary doctor, name, etc.)
+UPDATE Patients
+SET firstName = :firstNameInput, lastName = :lastNameInput, primaryDoctorID = :primaryDoctorIDInput
+WHERE patientID = :patientIDInput;
+
+--update a staff(change of type, name, etc.)
+UPDATE Staff
+SET firstName = :firstNameInput, lastName = :lastNameInput, staffType = :staffTypeInput
+WHERE staffID = :staffIDInput;
+
+--delete a doctor(quits job)
+DELETE FROM Doctors WHERE doctorID = :doctorIDInput;
+
+--delete a patient(switches hospital)
+DELETE FROM Patients WHERE patientID = :patientIDInput;
+
+--delete a staff(quits job)
+DELETE FROM Staff WHERE staffID = :staffIDInput;
+
+--delete doctor-patient relationship(patient leaves a doctor, vice versa)
+DELETE FROM Doctors_Patients WHERE doctorID = :doctorIDInput AND patientID = :patientIDInput
