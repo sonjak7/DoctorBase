dd1039028b96eaae02015e950ee95ee3ae8aa160
rosewasp <55768481+rosewasp@users.noreply.github.com>
2020-11-20 22:24:16 -0800

    multiple changes

--- hospital_base/backend.py
+++ hospital_base/backend.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
-from flask import request, redirect
+from flask import request,redirect
+from flask import flash
 from db_connector.db_connector import connect_to_database, execute_query
 #create the web application
 webapp = Flask(__name__)
-
+webapp.secret_key = 'secretkey'
 #provide a route where requests on the web application can be addressed
 @webapp.route('/')
 def home_page():
@@ -13,6 +14,7 @@ def home_page():
 def doctors():
     return render_template('doctors.html')
 
+
 @webapp.route('/patients')
 def patients():
     return render_template('patients.html')
@@ -20,6 +22,50 @@ def patients():
 @webapp.route('/staff')
 def staff():
     return render_template('staff.html')
+
+@webapp.route('/add_staff', methods=['POST','GET'])
+def add_staff():
+    db_connection = connect_to_database()
+    if request.method == 'GET':
+        return render_template('add_staff.html')
+    elif request.method == 'POST':
+        print("Add new people!")
+        fname = request.form['fname']
+        lname = request.form['lname']
+        type = request.form['type']
+
+        query = 'INSERT INTO staff (firstname, lastname, stafftype) VALUES (%s,%s,%s)'
+        data = (fname, lname, type)
+        execute_query(db_connection, query, data)
+        flash('Staff Added!')
+        return render_template('add_staff.html')
+
+#endpoint for search
+@webapp.route('/search_staff', methods=['GET', 'POST'])
+def search():
+    if request.method == "POST":
+
+        search_data = request.form['search_data']
+
+        db_connection = connect_to_database()
+        # search by firstName or lastName
+        query = "SELECT firstName, lastName, staffType, staffID from staff WHERE firstname LIKE %s OR lastName LIKE %s"
+        data = (search_data, search_data)
+        result = execute_query(db_connection, query, data).fetchall()
+        count = len(result)
+        flash(str(count) + " Staff Found!")
+        return render_template('browse_staff.html', rows=result)
+    return render_template('search_staff.html')
+
+@webapp.route('/browse_staff')
+#the name of this function is just a cosmetic thing
+def browse_staff():
+    print("Fetching and rendering Staff web page")
+    db_connection = connect_to_database()
+    query = "SELECT firstName, lastName, staffType, staffID from staff;"
+    result = execute_query(db_connection, query).fetchall()
+    print(result)
+    return render_template('browse_staff.html', rows=result)
 
 @webapp.route('/orders')
 def orders():
@@ -57,10 +103,6 @@ def doctors_patients():
 # def inquire_results():
 #     return render_template('inquire_results.html')
 
-# @webapp.route('/inquire_staff')
-# def inquire_staff():
-#     return render_template('inquire_staff.html')
-
 # @webapp.route('/add')
 # def add():
 #     return render_template('add.html')
@@ -77,9 +119,6 @@ def doctors_patients():
 # def add_orders():
 #     return render_template('add_orders.html')
 
-# @webapp.route('/add_staff')
-# def add_staff():
-#     return render_template('add_staff.html')
 
 # @webapp.route('/add_results')
 # def add_results():
@@ -88,5 +127,3 @@ def doctors_patients():
 # @webapp.route('/update')
 # def update():
 #     return render_template('update.html')
-
-
